1532eb58dc78cd9c8aea34e21fac7526f94c5f98
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-05-27 00:19:29 +0530

    Add oops code

---
+++ OOP/constructorWithPrototype.js
@@ -0,0 +1,18 @@
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+  constructor: Dog,
+  numLegs: 4,
+  eat: function () {
+    console.log("nom nom nom");
+  },
+  describe: function () {
+    console.log("My name is " + this.name);
+  },
+};
+
+const dg = new Dog("montu");
+console.log(dg);
